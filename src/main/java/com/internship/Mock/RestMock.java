d6ebbab3f18d5e2f04afb464238c2159530e4116
Maxim <mbarazgov02@gmail.com>
2025-01-09 10:31:18 +0300

    Added POST-method

--- src/main/java/com/internship/Mock/RestMock.java
+++ src/main/java/com/internship/Mock/RestMock.java
@@ -3,7 +3,12 @@ package com.internship.Mock;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class RestMock {
@@ -11,7 +16,19 @@ public class RestMock {
     @GetMapping("/get-json")
     public ResponseEntity<String> getStaticJSON() throws InterruptedException {
         Thread.sleep(1000 + (long)(Math.random() * 1000));
+
         String staticJSON = "{\"message\": \"Hey!\"}";
         return ResponseEntity.ok(staticJSON);
+    }
+
+    @PostMapping("/send-json")
+    public ResponseEntity<Map<String, String>> sendJSON(@RequestBody Map<String, String> request) throws InterruptedException {
+        Thread.sleep(1000 + (long)(Math.random() * 1000));
+
+        Map<String, String> response = new HashMap<>();
+        response.put("login", request.get("login"));
+        response.put("password", request.get("password"));
+        response.put("date", new Date().toString());
+        return ResponseEntity.ok(response);
     }
 }
