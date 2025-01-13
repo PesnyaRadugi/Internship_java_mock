c10ada28dde8fde428c41565846c4d0800267bc4
Maxim <mbarazgov02@gmail.com>
2025-01-13 15:24:00 +0300

    toString overriden, added annotations for validation

--- src/main/java/com/internship/Mock/User.java
+++ src/main/java/com/internship/Mock/User.java
@@ -1,16 +1,35 @@
 package com.internship.Mock;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import java.util.Date;
 
 public class User {
+
+    @NotBlank(message = "Login can't be empty")
     public String login;
-    public  String password;
+
+    @NotBlank(message = "Password can't be empty")
+    @Size(min = 3)
+    public String password;
+
     public Date date;
 
     public User(String login, String password) {
         this.login = login;
         this.password = password;
         this.date = new Date();
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "{\"login\":\"%s\"," +
+                        "\"password\": \"%s\"," +
+                        "\"date\": \"%s\"}",
+                login, password, date
+        );
     }
 
 }
